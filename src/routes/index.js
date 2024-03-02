2c3af21067267b7e936f81a1deda4289dc2bb10e
daisuke.okuda@accesslab.co.jp <Daisuke>
2024-03-02 15:07:06 +0900

    Deleting comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const mondayRoutes = require('./monday');
 const { body, validationResult } = require('express-validator');
-//const jwt_decode = require('jwt-decode');
-// const jwt_decode = require('jwt-decode').default;
-//const jwtDecode = require('jwt-decode');
 const jwt = require("jsonwebtoken");
 const fetch = require('node-fetch');
 
@@ -28,22 +25,14 @@ router.post('/isempty', [
     return value !== '' && value !== null;
   })
 ], (req, res) => {
-  //res.status(200).send({});
   const token = req.headers.authorization;
   const decoded = jwt.decode(token);
-  //const decoded = jwt.verify(token);
-  //  console.log("INPUT FIELDS:", req.body.payload.inputFields);
-  //console.log("token:",token);
-  console.log("SHORTLIVEDTOKEN:", decoded.shortLivedToken);
-  console.log("boardId", req.body.payload.inputFields.boardId);
-  console.log("itemId",req.body.payload.inputFields.itemId)
+  // console.log("SHORTLIVEDTOKEN:", decoded.shortLivedToken);
 
   const errors = validationResult(req);
   let validationMessage;
 
   if (!errors.isEmpty()) {
-    //validationMessage = "未入力です。値を入力してください";
-    //validationMessage = "Null or Empty";
     validationMessage = "未入力です。値を入力してください";
     console.log("未入力です。値を入力してください");
     //return res.status(400).json({ errors: errors.array() });
@@ -51,50 +40,16 @@ router.post('/isempty', [
   }
   else
   {
-    //validationMessage = "値が正常に入力されました";
-    // validationMessage = "OK";
     validationMessage = "値が正常に入力されました";
     console.log("値が正常に入力されました");
   }
-  //res.sendStatus(200);
 
-  //const textValue = JSON.stringify(validationMessage);
-   //const query = `mutation {change_simple_column_value(board_id: ${req.body.payload.inputFields.boardId}, item_id: ${req.body.payload.inputFields.itemId}, column_id: "text5", value: "${validationMessage}") {id}}`;
-   //---------------- コメントアウトここから
-   //  const query = {'query' : `mutation {change_simple_column_value(board_id: ${req.body.payload.inputFields.boardId}, item_id: ${req.body.payload.inputFields.itemId}, column_id: "text5", value: "${validationMessage}") {id}}`};
-  //  console.log(query);
-
-  // fetch ("http://api.monday.com/v2",{
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Authorization': decoded.shortLivedToken
-  //     //'Authorization': `Bearer ${your_auth_token}`
-  //   },
-  //    body: JSON.stringify({
-  //      'query' : `mutation {change_simple_column_value(board_id: ${req.body.payload.inputFields.boardId}, item_id: ${req.body.payload.inputFields.itemId}, column_id: "text5", value: "${validationMessage}") {id}}`
-  //    })
-  // })
-  // .then(response => response.json())
-  // .then(data => {
-  //   console.log(data);
-  //   res.status(200).send(data); // GraphQLの応答をクライアントに送信
-  // })
-  // .catch(error => {
-  //   console.error('Error:', error);
-  //   res.status(500).send({ message: '内部サーバーエラー' });
-  // });
-  //res.sendStatus(200)
-  //res.status(200).send({});
-
-  //---------------- コメントアウトここまで
-//---------------- 提案されたコードここから
   let myHeaders = new Headers(); 
   myHeaders.append("Authorization", decoded.shortLivedToken); 
   myHeaders.append("Content-Type", "application/json"); 
    
   let queryTwo = JSON.stringify({ 
-    "query": `mutation {change_simple_column_value(board_id: ${req.body.payload.inputFields.boardId}, item_id: ${req.body.payload.inputFields.itemId}, column_id: "text5", value: "${validationMessage}") {id}}` 
+      "query": `mutation {change_simple_column_value(board_id: ${req.body.payload.inputFields.boardId}, item_id: ${req.body.payload.inputFields.itemId}, column_id: "${req.body.payload.inputFields.targetColumnId}", value: "${validationMessage}") {id}}` 
   }); 
    
   let requestOptions = { 
@@ -108,18 +63,8 @@ router.post('/isempty', [
     .then(response => response.text()) 
     .then(result => console.log(result)) 
     .catch(error => console.log('error', error)) 
-//---------------- 提案されたコードここまで
-
 });
 
-// .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error('Error:', error));
-
-//   // レスポンスを送信
-//   //res.status(200).send({ message: validationMessage });
-//   res.sendStatus(200);
-// })
 function getHealth() {
   return {
     ok: true,
