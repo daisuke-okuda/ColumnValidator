328aabd0b44d4f62bc57366cfc4b9ee8e8a0045a
daisuke.okuda@accesslab.co.jp <Daisuke>
2024-03-18 17:29:52 +0900

    Adding contains

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,12 +9,12 @@ router.use(mondayRoutes);
 // router.get('/', function (req, res) {
 //   res.json(getHealth());
 // });
-
 // router.get('/health', function (req, res) {
 //   res.json(getHealth());
 //   res.end();
 // });
 
+// Text（テキスト列）の空文字 or Null チェック テキスト列の値がオブジェクト型でやって来るので、普通の
 router.post(
   '/isempty',
   [
@@ -65,9 +65,77 @@ router.post(
     };
 
     fetch('https://api.monday.com/v2', requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log('error', error));
+      .then((response) => response.json()) // ここでJSON形式でレスポンスを解析
+      .then((result) => {
+        // 成功した結果をクライアントに返す
+        res.json({ success: true, result: result });
+      })
+      .catch((error) => {
+        // エラーがあればクライアントにエラー情報を返す
+        console.log('error', error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+      });
+    // .then((response) => response.text())
+    // .then((result) => console.log(result))
+    // .catch((error) => console.log('error', error));
+  }
+);
+
+router.post(
+  '/Contains',
+  [
+    body('payload.inputFields.columnValue').custom((value, { req }) => {
+      const searchText = req.body.payload.inputFields.SearchTextValue;
+      if (!value.value.includes(searchText)) {
+        throw new Error(`columnValue must contain "${searchText}"`);
+      }
+      return true; // バリデーション成功
+    }),
+  ],
+  (req, res) => {
+    const token = req.headers.authorization;
+    const decoded = jwt.decode(token);
+
+    const errors = validationResult(req);
+    let validationMessage;
+
+    if (!errors.isEmpty()) {
+      validationMessage = req.body.payload.inputFields.FailTextValue;
+    } else {
+      // バリデーション成功時の処理
+      // res.send('Validation passed');
+      validationMessage = req.body.payload.inputFields.SuccessTextValue;
+      console.log('値が正常に入力されました');
+    }
+    let myHeaders = new Headers();
+    myHeaders.append('Authorization', decoded.shortLivedToken);
+    myHeaders.append('Content-Type', 'application/json');
+
+    let query = JSON.stringify({
+      query: `mutation {change_simple_column_value(board_id: ${req.body.payload.inputFields.boardId}, item_id: ${req.body.payload.inputFields.itemId}, column_id: "${req.body.payload.inputFields.targetColumnId}", value: "${validationMessage}") {id}}`,
+    });
+
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: query,
+      redirect: 'follow',
+    };
+
+    fetch('https://api.monday.com/v2', requestOptions)
+      .then((response) => response.json()) // ここでJSON形式でレスポンスを解析
+      .then((result) => {
+        // 成功した結果をクライアントに返す
+        res.json({ success: true, result: result });
+      })
+      .catch((error) => {
+        // エラーがあればクライアントにエラー情報を返す
+        console.log('error', error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+      });
+    // .then((response) => response.text())
+    // .then((result) => console.log(result))
+    // .catch((error) => console.log('error', error));
   }
 );
 
